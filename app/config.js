64b10229f5c39b0e8f5ed6c3360f8aabb1393788
Matt Girdler <matt.girdler@digital.landregistry.gov.uk>
2018-08-09 16:13:48 +0100

    Add 'Search for a property' page

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Digital Street',
+  serviceName: 'Sell land or property',
 
   // Default port that prototype runs on
   port: '3000',
