eccd784c011da7bdf2a38aa552f7c5557070a3ca
Matt Girdler <matt.girdler@digital.landregistry.gov.uk>
2018-08-14 11:42:42 +0100

    Add seller LR checks page. Start role-based views of property logbook

--- app/views/v0/routes.js
+++ app/views/v0/routes.js
@@ -1,8 +1,36 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 const router = express.Router()
 
 router.get('/foo', function(req, res) {
     res.send('Do weird logic here if you want')
+})
+
+router.get('/property-logbook', function(req, res) {
+    const propertyJSON = JSON.parse(fs.readFileSync('../../../data/v0-property.json'))
+    const lastURL = req.headers.referer
+    let role = 'anonymous'
+    if (lastURL) {
+        const lastEndpoint = lastURL.substr(lastURL.lastIndexOf('/') + 1)
+        console.log(lastEndpoint)
+        if (lastEndpoint == 'seller-land-registry-checks') {
+            role = 'owner'
+        }
+    }
+    res.render(path.resolve(__dirname, './property-logbook.html'), { role: role, property: propertyJSON })
+})
+
+router.get('/identify-sign-in', function(req, res) {
+    const lastURL = req.headers.referer
+    const nextURL = 'seller-land-registry-checks'
+    if (lastURL) {
+        const lastEndpoint = lastURL.substr(lastURL.lastIndexOf('/') + 1)
+        if (lastEndpoint == 'buyer-make-an-offer') {
+            nextURL = 'buyer-verify-agreement-in-principle'
+        } 
+    }
+    res.render(path.resolve(__dirname, './identify-sign-in.html'), { nextURL: nextURL })
 })
 
 module.exports = router
