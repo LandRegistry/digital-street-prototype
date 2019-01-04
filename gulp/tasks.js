b69117a0ebb781575b1f7dc9b4ce5540e4ebbe4f
Matt Girdler <matt.girdler@digital.landregistry.gov.uk>
2019-01-04 09:16:49 +0000

    Feature/conveyancer lender mobile responsive (#3)
    * Make buyer/seller pages mobile responsive
    
    * Refactor case list pages
    
    * Add gulp task to include bootstrap js and fix navbar toggle. Make remaining conveyancer/lender pages mobile responsive
    
    * address formatting. design tweaks
    
    * Remove commented line

--- gulp/tasks.js
+++ gulp/tasks.js
@@ -7,6 +7,9 @@
 const gulp = require('gulp')
 const mocha = require('gulp-mocha')
 const runSequence = require('run-sequence')
+const sourcemaps = require('gulp-sourcemaps')
+
+const config = require('./config.json')
 
 gulp.task('default', function (done) {
   runSequence('generate-assets',
@@ -17,6 +20,7 @@ gulp.task('default', function (done) {
 gulp.task('generate-assets', function (done) {
   runSequence('clean',
     'sass',
+    'js',
     'copy-assets',
     'sass-documentation',
     'copy-assets-documentation',
@@ -41,3 +45,9 @@ gulp.task('mocha', function () {
     .pipe(mocha({ reporter: 'spec', exit: true }))
     .on('error', console.error)
 })
+
+
+gulp.task('js', function() {  
+  return gulp.src([config.paths.nodeModules + '/bootstrap/dist/js/bootstrap.min.js', config.paths.nodeModules + '/jquery/dist/jquery.min.js'])
+    .pipe(gulp.dest(config.paths.public + '/javascripts/'))      
+});
